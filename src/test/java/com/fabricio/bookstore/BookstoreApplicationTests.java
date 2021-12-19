c5a78215ec709c84791ffe6ebbcfcee013365fff
fabricoliveira <oliveira.fabricio.santos@gmail.com>
2021-12-19 00:38:32 -0300

    commit inicial

---
+++ src/test/java/com/fabricio/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabricio.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
