294a825ec68e6feb9ccb354c9bea6af4a183d128
fabricoliveira <oliveira.fabricio.santos@gmail.com>
2021-12-19 21:19:25 -0300

    Utilização da exception ResourceNotFoundException

--- src/main/java/com/fabricio/bookstore/services/CategoriaService.java
+++ src/main/java/com/fabricio/bookstore/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.fabricio.bookstore.services;
 
 import com.fabricio.bookstore.domain.Categoria;
+import com.fabricio.bookstore.exceptions.ResourceNotFoundException;
 import com.fabricio.bookstore.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,6 @@ public class CategoriaService {
     private CategoriaRepository categoriaRepository;
 
     public Categoria findById(Integer id) {
-        return categoriaRepository.findById(id).orElse(null);
+        return categoriaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("O recurso não foi encontrado! ID: " + id + ", TIPO: " + Categoria.class.getName()));
     }
 }
