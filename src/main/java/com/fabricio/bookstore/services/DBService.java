f513b930ec2118e3dfc13c6eb8f70021c6f72b30
fabricoliveira <oliveira.fabricio.santos@gmail.com>
2021-12-19 21:15:49 -0300

    Remoção do @Lombok.Builder e criação do objeto via construtor

--- src/main/java/com/fabricio/bookstore/services/DBService.java
+++ src/main/java/com/fabricio/bookstore/services/DBService.java
@@ -20,15 +20,15 @@ public class DBService {
     private LivroRepository livroRepository;
 
     public void inicializaBaseDeDados() {
-        Categoria cat1 = Categoria.builder().nome("Informática").descricao("Livros de TI").livros(new ArrayList<>()).build();
-        Categoria cat2 = Categoria.builder().nome("Ficção Científica").descricao("Livros de Ficção Científica").livros(new ArrayList<>()).build();
-        Categoria cat3 = Categoria.builder().nome("Biografia").descricao("Livros de Biografias").livros(new ArrayList<>()).build();
+        Categoria cat1 = new Categoria(null, "Informática", "Livros de TI", new ArrayList<>());
+        Categoria cat2 = new Categoria(null, "Ficção Científica", "Livros de Ficção Científica", new ArrayList<>());
+        Categoria cat3 = new Categoria(null, "Biografia", "Livros de Biografias", new ArrayList<>());
 
-        Livro l1 = Livro.builder().titulo("Clean Code").nomeAutor("Robert Martin").texto("Lorem Ipsum").categoria(cat1).build();
-        Livro l2 = Livro.builder().titulo("Engenharia de Software").nomeAutor("Luis V. Gerstner").texto("Lorem Ipsum").categoria(cat1).build();
-        Livro l3 = Livro.builder().titulo("The Time Machine").nomeAutor("H.G Wells").texto("Lorem Ipsum").categoria(cat2).build();
-        Livro l4 = Livro.builder().titulo("The War of the Worlds").nomeAutor("H.G Wells").texto("Lorem Ipsum").categoria(cat2).build();
-        Livro l5 = Livro.builder().titulo("I, Robot").nomeAutor("Isaac Asimov").texto("Lorem Ipsum").categoria(cat2).build();
+        Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem Ipsum", cat1);
+        Livro l2 = new Livro(null, "Engenharia de Software", "Luis V. Gerstner", "Lorem Ipsum", cat1);
+        Livro l3 = new Livro(null, "The Time Machine", "H.G Wells", "Lorem Ipsum", cat2);
+        Livro l4 = new Livro(null, "The War of the Worlds", "H.G Wells", "Lorem Ipsum", cat2);
+        Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem Ipsum", cat2);
 
         cat1.getLivros().addAll(Arrays.asList(l1, l2));
         cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
