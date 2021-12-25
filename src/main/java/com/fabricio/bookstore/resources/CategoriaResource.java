8846e4865c26d0ed0bda4b217f45e6761db1ec9c
fabricoliveira <oliveira.fabricio.santos@gmail.com>
2021-12-25 16:35:26 -0300

    Adicionando o CrossOrigin nos endpoints para ser consumido pelo Angular.

--- src/main/java/com/fabricio/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/fabricio/bookstore/resources/CategoriaResource.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
--- src/main/java/com/fabricio/bookstore/resources/LivroResource.java
+++ src/main/java/com/fabricio/bookstore/resources/LivroResource.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
