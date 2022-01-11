88e13e65096f47167cb8e29da9da4590456d27af
fabricoliveira <oliveira.fabricio.santos@gmail.com>
2022-01-11 03:35:46 -0300

    Correção no CORS

--- src/main/java/com/fabricio/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/fabricio/bookstore/resources/CategoriaResource.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
--- src/main/java/com/fabricio/bookstore/resources/LivroResource.java
+++ src/main/java/com/fabricio/bookstore/resources/LivroResource.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
